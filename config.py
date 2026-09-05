b57ba9981ff2cee0adac19ba328006d683a45844
AnimeshLPU <tripathianimesh38@gmail.com>
2026-09-05 10:25:19 +0530

    Upgrade: Switch to Whisper-Large-v3-Turbo and Llama-3.3-70B in config.py

--- config.py
+++ config.py
@@ -13,9 +13,9 @@ HF_MODELS = {
     "emotion_classifier": "SamLowe/roberta-base-go_emotions",
     "distress_zero_shot": "facebook/bart-large-mnli",
     "sentiment_classifier": "cardiffnlp/twitter-roberta-base-sentiment-latest",
-    "asr_speech_to_text": "openai/whisper-small",
+    "asr_speech_to_text": "openai/whisper-large-v3-turbo",
     "audio_emotion": "superb/wav2vec2-base-superb-er",
-    "reasoning_llm": "meta-llama/Llama-3.2-3B-Instruct"
+    "reasoning_llm": "meta-llama/Llama-3.3-70B-Instruct"
 }
 
 # Standard Risk Tier Vocabulary (as defined in SIH 26094 documentation)
